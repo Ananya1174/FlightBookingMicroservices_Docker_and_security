ea9ecac700d34f28171183d55d17d4be56d5d5cf
Ananya1174 <ananyaarisenapalli@gmail.com>
2025-12-01 19:41:02 +0530

    updated FlightResponseDto.java

--- flight-service/src/main/java/com/flightservice/dto/FlightResponseDto.java
+++ flight-service/src/main/java/com/flightservice/dto/FlightResponseDto.java
@@ -1,7 +1,15 @@
 package com.flightservice.dto;
 
 import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class FlightResponseDto {
 
     private Long id;
@@ -15,72 +23,4 @@ public class FlightResponseDto {
     private Double price;
     private String tripType;
     private Integer totalSeats;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getFlightNumber() {
-		return flightNumber;
-	}
-	public void setFlightNumber(String flightNumber) {
-		this.flightNumber = flightNumber;
-	}
-	public String getAirlineName() {
-		return airlineName;
-	}
-	public void setAirlineName(String airlineName) {
-		this.airlineName = airlineName;
-	}
-	public String getAirlineLogoUrl() {
-		return airlineLogoUrl;
-	}
-	public void setAirlineLogoUrl(String airlineLogoUrl) {
-		this.airlineLogoUrl = airlineLogoUrl;
-	}
-	public String getOrigin() {
-		return origin;
-	}
-	public void setOrigin(String origin) {
-		this.origin = origin;
-	}
-	public String getDestination() {
-		return destination;
-	}
-	public void setDestination(String destination) {
-		this.destination = destination;
-	}
-	public LocalDateTime getDepartureTime() {
-		return departureTime;
-	}
-	public void setDepartureTime(LocalDateTime departureTime) {
-		this.departureTime = departureTime;
-	}
-	public LocalDateTime getArrivalTime() {
-		return arrivalTime;
-	}
-	public void setArrivalTime(LocalDateTime arrivalTime) {
-		this.arrivalTime = arrivalTime;
-	}
-	public Double getPrice() {
-		return price;
-	}
-	public void setPrice(Double price) {
-		this.price = price;
-	}
-	public String getTripType() {
-		return tripType;
-	}
-	public void setTripType(String tripType) {
-		this.tripType = tripType;
-	}
-	public Integer getTotalSeats() {
-		return totalSeats;
-	}
-	public void setTotalSeats(Integer totalSeats) {
-		this.totalSeats = totalSeats;
-	}
-
-    // getters/setters and constructors
 }
